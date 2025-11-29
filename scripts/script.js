e790944801f18dbfc35511b623712660dc97f0f0
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-11-29 18:22:46 +0100

    Slide nav bar in and out isntead of hiding/showing

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 function nav_bar_toggle(event) {
     let nav = document.getElementById("nav");
-    nav.classList.toggle("hidden");
+    //nav.classList.toggle("hidden");
+
+    nav.classList.toggle("lg:translate-x-0")
+    nav.classList.toggle("translate-y-0")
+
 }
 
 let menuBtn = document.getElementById("menuBtn");
