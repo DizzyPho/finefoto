ef262b48c6e8b6852a5bc3bc696b0f8f6b0ac236
dizzypho <184835166+DizzyPho@users.noreply.github.com.>
2025-11-30 15:42:56 +0100

    Remove commented code

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 function navBarToggle(event) {
     let nav = document.getElementById("nav");
-    //nav.classList.toggle("hidden");
 
     nav.classList.toggle("lg:translate-x-0")
     nav.classList.toggle("translate-y-0")
