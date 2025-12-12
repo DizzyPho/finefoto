0e0d42922b285e780e8deec796c441ecc12cb0b3
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-12-12 10:07:52 +0100

    Open overlay by clicking on image

---
+++ scripts/enlarge.js
@@ -0,0 +1,20 @@
+let galleryImages = document.getElementsByClassName("gallery-img")
+
+for(image of galleryImages) {
+    image.addEventListener("click",openOverlay);
+}
+
+function closeOverlay(event) {
+    const overlay = document.getElementById("overlay");
+    overlay.remove();
+}
+function openOverlay(event) {
+    const overlay = document.createElement("div");
+    overlay.id = "overlay";
+    const x = document.createElement("button");
+    x.id = "x";
+    x.innerHTML=`<img src="/images/ui/x.png" alt="x">`
+    x.addEventListener("click", closeOverlay)
+    overlay.appendChild(x);
+    document.body.appendChild(overlay)
+}
